9277d89d5289b38cb175c8e65a712befe68050d1
Prateek Mulye <prateekmulye@hotmail.com>
2025-11-28 14:09:10 -0800

    feat(LangGraph): LangGraph

--- nodes.py
+++ nodes.py
@@ -6,14 +6,18 @@ from react import llm, tools
 
 load_dotenv()
 
-SYSTEM_MESSAGE="""
+SYSTEM_MESSAGE = """
 You are a helpful assistant that can use tools to answer questions."""
+
 
 def run_agent_reasoning(state: MessagesState) -> MessagesState:
     """
     Run the agent reasoning node.
     """
-    response = llm.invoke([{"role": "system", "content": SYSTEM_MESSAGE}, *state["messages"]])
+    response = llm.invoke(
+        [{"role": "system", "content": SYSTEM_MESSAGE}, *state["messages"]]
+    )
     return {"messages": [response]}
+
 
 tool_node = ToolNode(tools=tools)
--- react.py
+++ react.py
@@ -5,13 +5,15 @@ from langchain_tavily import TavilySearch
 
 load_dotenv()
 
+
 @tool
-def triple(num:float) -> float:
+def triple(num: float) -> float:
     """
     param num: a number to triple
     returns: the triple of the input number
     """
     return float(num) * 3
+
 
 tools = [TavilySearch(max_results=1), triple]
 llm = ChatOpenAI(model="gpt-4o", temperature=0).bind_tools(tools)
